fc1d76847730210ca70f9c642e7da01aec5401a4
NesteaHub <veselova99@mail.ru>
2019-11-30 20:59:28 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(57, 95);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(596, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(142, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(442, 21);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Enter a number from \"one\" to \"nine hundred ninety nine\"\r\n";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(57, 164);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(84, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(569, 164);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(84, 20);
+            this.textBox3.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 10F);
+            this.label2.Location = new System.Drawing.Point(55, 145);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 19);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Arabic num";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 10F);
+            this.label3.Location = new System.Drawing.Point(565, 145);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(94, 19);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Roman num";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(255, 224);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(218, 39);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Show";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(225)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(709, 275);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.Name = "Form1";
+            this.Text = "Numbers";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,244 @@
+﻿using System.Text;
+using System.Threading.Tasks;
+
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System;
+using System.Linq;
+
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = "";
+            textBox3.Text = "";
+            string text = textBox1.Text;
+            string[] slovar1 = { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+            string[] slovar2 = { "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen" };
+            string[] slovar3 = {"twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"};
+            string slovar4 = "hundred";
+
+            string [] words;
+            char[] splittext = { ' ' };
+            words = text.Split(splittext, StringSplitOptions.RemoveEmptyEntries);
+            words = words.ToArray();
+
+            int i = words.Length-1;
+            int output = 0;
+
+            if (words.Length < 1) 
+                MessageBox.Show("Ошибка: введите число");
+
+            
+
+            while (i >= 0)
+            {
+                for (int j = 0; j < words.Length; j++)
+                {
+                    if ((!slovar1.Contains(words[j])) && (!slovar2.Contains(words[j])) && (!slovar3.Contains(words[j])) 
+                        && (slovar4 != words[j]))
+                    {
+                        MessageBox.Show("Ошибка в слове " + words[j]);
+                        i = -1;
+                        output = 0;
+                        break;
+                    }
+                }           ////проверка на ввод не слов
+                  
+                if (i < 0) break;
+
+                for (int j1 = 0; j1 < slovar1.Length; j1++)
+                {
+                    if ((words[i] == slovar1[j1]) && (i - 1 >= 0))
+                    {
+                        if (slovar2.Contains(words[i - 1]))
+                        {
+                            MessageBox.Show("Ошибка: числа разной разрядности (" + words[i - 1] + " не может располагаться перед " + words[i] + ")");
+                            i = -1;
+                            output = 0;
+                            break;
+                        }
+                        else if (slovar1.Contains(words[i - 1]))
+                        {
+                            MessageBox.Show("Ошибка: числа одинаковой разрядности (" + words[i - 1] + " не может располагаться перед " + words[i] + ")");
+                            i = -1;
+                            output = 0;
+                            break;
+                        }
+                        output += j1 + 1;
+                        i--;
+                        break;
+                    }
+                    else if ((words[0] == slovar1[j1]) && (words.Length == 1))
+                    {
+                        output += j1 + 1;
+                        i--;
+                        break;
+                    }
+                }         ////словарь1
+
+                if (i < 0) break;
+
+                for (int k = 0; k < slovar2.Length; k++)
+                {
+                    if ((words[i] == slovar2[k]) && (i - 1 >= 0))
+                    {
+                        if (slovar1.Contains(words[i - 1]))
+                        {
+                            MessageBox.Show("Ошибка: числа разной разрядности (" + words[i - 1] + " не может располагаться перед " + words[i] + ")");
+                            i = -1;
+                            output = 0;
+                            break;
+                        }
+                        if (slovar2.Contains(words[i - 1]))
+                        {
+                            MessageBox.Show("Ошибка: числа одинаковой разрядности (" + words[i - 1] + " не может располагаться перед " + words[i] + ")");
+                            i = -1;
+                            output = 0;
+                            break;
+                        }
+                        if (slovar3.Contains(words[i - 1]))
+                        {
+                            MessageBox.Show("Ошибка: числа разной разрядности (" + words[i - 1] + " не может располагаться перед " + words[i] + ")");
+                            i = -1;
+                            output = 0;
+                            break;
+                        }
+                        output += k + 10;
+                        i--;
+                        break;
+                    }                   
+                    else if ((words[0] == slovar2[k]) && (words.Length == 1))
+                    {
+                        output += k + 10;
+                        i--;
+                        break;
+                    }
+                }           ////словарь2
+
+                if (i < 0) break;
+
+                for (int j2 = 0; j2 < slovar3.Length; j2++)
+                {
+                    if ((words[i] == slovar3[j2]) && (i - 1 >= 0))
+                    {
+                        if (slovar3.Contains(words[i - 1]))
+                        {
+                            MessageBox.Show("Ошибка: числа одинаковой разрядности (" + words[i - 1] + " не может располагаться перед " + words[i] + ")");
+                            i = -1;
+                            output = 0;
+                            break;
+                        }
+                        else if (slovar2.Contains(words[i - 1]))
+                        {
+                            MessageBox.Show("Ошибка: числа разной разрядности (" + words[i - 1] + " не может располагаться перед " + words[i] + ")");
+                            i = -1;
+                            output = 0;
+                            break;
+                        }
+                        else if (slovar1.Contains(words[i - 1]))
+                        {
+                            MessageBox.Show("Ошибка: числа разной разрядности (" + words[i - 1] + " не может располагаться перед " + words[i] + ")");
+                            i = -1;
+                            output = 0;
+                            break;
+                        }
+                        output += (j2 + 2) * 10;
+                        i--;
+                        break;
+                    }
+                    else if (words[0] == slovar3[j2]) 
+                    {
+                        output += (j2 + 2) * 10;
+                        i--;
+                        break;
+                    }                   
+                }           ////словарь3
+
+                if (i < 0) break;
+                if (words[0] == slovar4)
+                {
+                    MessageBox.Show("Ошибка: укажите число сотен");
+                    i = -1;
+                    output = 0;
+                    break;
+                }
+                if (i < 0) break;
+                
+                if (words.Length >= 2)
+                {
+                    for (int l = 0; l < slovar1.Length; l++)
+                    {
+                        if ((words[1] == slovar4)&&((slovar2.Contains(words[0]))||(slovar3.Contains(words[0]))))
+                        {
+                            MessageBox.Show("Ошибка: число сотен не должно превышать девяти");
+                            i = -1;
+                            output = 0;
+                            break;
+                        }
+                        if (i < 0) break;
+
+                        else if ((words[1] == slovar4) && (words[0] == slovar1[l]))
+                        {
+                            output += (l + 1) * 100;
+                            i--; i--;
+                            break;
+                        }    
+                        else if(words[1] != slovar4)
+                        {
+                            MessageBox.Show("Ошибка: перед сотней должно располагаться одно число от 1 до 9");
+                            i = -1;
+                            output = 0;
+                            break;
+                        }
+                    }           ////сотни 
+                }
+                if (i < 0) break;
+
+            }
+            int count = 0;
+            for (int z = 0; z < words.Length; z++)
+            {
+                if (words[z] == "hundred")
+                {
+                    count++;
+                }
+            }
+            if (count > 1)
+            {
+                MessageBox.Show("Ошибка: слово 'hundred' не может повторяться");
+                i = -1;
+                output = 0;
+            }
+            if (output > 0)
+                textBox2.Text = output.ToString();
+
+            string[] rim = { "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
+            int[] arab = { 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+            int c = 0;
+            string output2 = "";
+
+            while (output > 0)
+            {
+                if (arab[c] <= output)
+                {
+                    output -= arab[c];
+                    output2 += rim[c];
+                }
+                else c++;
+            }
+            textBox3.Text = output2;
+        }
+    }
+}
